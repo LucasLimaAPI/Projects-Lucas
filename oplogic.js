2b87121affcb9e5c3705bb52b04602287b617ffd
Lucas Lima de Oliveira <lllima3780@gmail.com>
2022-01-13 00:08:57 -0300

    JavScript

---
+++ oplogic.js
@@ -0,0 +1,38 @@
+/*
+operadores de comparação
+> maior
+>= maior que ou igual a
+< menor
+<= menor que ou igual a 
+== igualdade(valor)
+=(atribuição) 
+=== igualdade estrita (valor e tipo) USAR ESSE
+!= diferente (valor)
+!==diferente estrito(valor e tipo)USAR ESSE 
+*/
+const maiorQue = 10 > 5;
+console.log(maiorQue);
+
+const maiorEqual = 10 >= 5;
+console.log(maiorEqual);
+
+const num1= 10
+const num2= 11
+
+const menorQue = num1 < num2;
+console.log(menorQue);
+
+const menorEqual = 5 <= 5;
+console.log(menorEqual);
+
+const equal = 5 == 5;
+console.log(equal);
+
+const superEqual = 10 === '10';
+console.log(superEqual);
+
+const dif = 10 != 5;
+console.log(dif);
+
+const superDif = 10 !== 5;
+console.log(superDif);
