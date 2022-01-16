44f3fbaa80edc92582be31a282025e8f6ce6537e
Lucas Lima de Oliveira <lllima3780@gmail.com>
2022-01-16 13:45:31 -0300

    JavaScript

---
+++ aulas projetos/aula5/else_if.js
@@ -0,0 +1,11 @@
+const number = 13;
+
+if (number >= 0 && number <= 5) {
+    console.log('O numero está entre 0 e 5 ');
+}else if(number >= 6 && number <= 8){
+    console.log('O numero está entre 6 e 8');
+}else if(number >= 9 && number <= 11){
+    console.log('O numero está entre 9 e 11');
+}else {
+    console.log('O numero que você escolheu está incorreto !!')
+}
---
+++ aulas projetos/aula5/index.js
@@ -0,0 +1,27 @@
+/*
+Entre 0 - 11 = bom dia
+Entre 12 - 17 = boa tarde
+Entre 18 - 23 = boa noite
+*/
+
+
+const hora = 22;
+
+if (hora >= 0 && hora <= 11) {
+    console.log('Bom dia Lucas ');
+} else if (hora >= 12 && hora <= 17){
+    console.log('Boa tarde Lucas');
+} else if (hora >= 18 && hora <=23){
+    console.log('Boa noite Lucas')
+} else{
+    console.log('Hora incorreta')
+}
+
+const money = false ;
+if (money){
+    console.log('Vou sair de casa')
+}else{
+    console.log('Não vou sair de casa')
+}
+
+
